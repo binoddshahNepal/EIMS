1b993b842ce62437e24623a61b78ff73b2ea20b5
Ashok Bhattarai <eashokbhattarai@gmail.com>
2023-03-18 08:48:08 +0545

    changes pushed

---
+++ EIMS/EIMS/Controllers/usersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EIMS.Models;
+
+namespace EIMS.Controllers
+{
+    public class usersController : Controller
+    {
+        private readonly employeeContext _context;
+
+        public usersController(employeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: users
+        public async Task<IActionResult> Index()
+        {
+              return _context.users != null ? 
+                          View(await _context.users.ToListAsync()) :
+                          Problem("Entity set 'employeeContext.users'  is null.");
+        }
+
+        // GET: users/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.users == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.users
+                .FirstOrDefaultAsync(m => m.userId == id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+        // GET: users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("userId,userName,password,role")] users users)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(users);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(users);
+        }
+
+        // GET: users/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.users == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("userId,userName,password,role")] users users)
+        {
+            if (id != users.userId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(users);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!usersExists(users.userId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(users);
+        }
+
+        // GET: users/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.users == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.users
+                .FirstOrDefaultAsync(m => m.userId == id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
+        }
+
+        // POST: users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.users == null)
+            {
+                return Problem("Entity set 'employeeContext.users'  is null.");
+            }
+            var users = await _context.users.FindAsync(id);
+            if (users != null)
+            {
+                _context.users.Remove(users);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool usersExists(int id)
+        {
+          return (_context.users?.Any(e => e.userId == id)).GetValueOrDefault();
+        }
+    }
+}
