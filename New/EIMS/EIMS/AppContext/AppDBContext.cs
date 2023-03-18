a5888c5a253e399db19ecaef1cdab2da3478ce7b
Ashok Bhattarai <eashokbhattarai@gmail.com>
2023-03-18 08:38:44 +0545

    added new project

---
+++ New/EIMS/EIMS/AppContext/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿
+using EIMS.Models;
+using Microsoft.EntityFrameworkCore;  
+  
+namespace EIMS.AppContext;
+
+public class AppDBContext : DbContext
+{
+    public AppDBContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
